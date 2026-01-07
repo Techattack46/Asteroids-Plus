f7eaec755fc4cb7264aa4ca60902591a5df3e91e
Techattack46 <bigvyg@gmail.com>
2026-01-07 13:12:17 +0100

    changed menu screen & death sound functionality

--- AsteroidsGDES2526_STARTER - kopie/Assets/Scripts/BossContact.cs
+++ AsteroidsGDES2526_STARTER - kopie/Assets/Scripts/BossContact.cs
@@ -2,10 +2,13 @@ using UnityEngine;
 
 public class BossContact : MonoBehaviour
 {
+    public AudioClip deathSound;
+    
     private void OnTriggerEnter(Collider other)
     {
         if (other.gameObject.CompareTag("Enemy"))
         {
+            AudioManager.Instance.PlayClip(deathSound);
             ContactDestroy.PlayerDeath();
         }
     }
